074229152232ab8fe7f13ba485970aefc45d9ff6
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-07-05 00:02:38 +0530

    Add Way Too Long Words [A] from Codeforces

---
+++ 71/A - Way Too Long Words/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    while (n--) {
+        string s;
+        cin>>s;
+        if (s.length()<=10) {
+            cout << s << endl;
+        } else {
+            cout<<s[0]<<s.length() - 2<<s[s.length() - 1]<<endl;
+        }
+    }
+    return 0;
+}

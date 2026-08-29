2c2e9a57f6c5ea44ec49e95d16ba4660e86a4db1
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-08-29 20:43:40 +0530

    Add Odd Eraser [A] from Codeforces

---
+++ 2258/A - Odd Eraser/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<long long> a(n);
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+        }
+        cout<<gcd(a[0], a[n-1])<<'
+';
+    }
+    return 0;
+}

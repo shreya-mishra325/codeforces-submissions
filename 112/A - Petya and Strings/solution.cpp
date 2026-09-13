dee216a04e86bb61296093797262e669ba6738c1
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-09-13 18:22:12 +0530

    Add Petya and Strings [A] from Codeforces

---
+++ 112/A - Petya and Strings/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	string s1, s2;
+	cin>>s1>>s2;
+	for(int i=0; i<s1.size(); i++){
+	    if(tolower(s1[i])<tolower(s2[i])) {
+	        cout<<-1;
+	        return 0;
+	    }
+	    else if(tolower(s2[i])<tolower(s1[i])){
+	        cout<<1;
+	        return 0;
+	    } 
+	}
+    cout<<0;
+}
